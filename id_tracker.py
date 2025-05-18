7e80f447ef9e5557107fb43497a37028727f2d3d
ldx <ldx@xiaofutech.com>
2025-05-18 19:25:25 +0200

    refactor(feature_extractor, id_tracker, people_extractor): clean up code by commenting out unused features and adjusting model configurations

--- id_tracker.py
+++ id_tracker.py
@@ -13,6 +13,7 @@ class TrackedObject:
     bbox: list
     last_seen: int
     features: torch.Tensor
+    features_buffer:list[torch.Tensor] = None
 
 
 class IDTracker:
@@ -45,7 +46,8 @@ class IDTracker:
             id=self.next_id,
             bbox=box,
             last_seen=self.now_seen,
-            features=features
+            features=features,
+            features_buffer=[features]
         )
         self.next_id += 1
     
@@ -53,7 +55,11 @@ class IDTracker:
         # Update the existing tracked object
         self.tracked_objects[obj_id].bbox = box
         self.tracked_objects[obj_id].last_seen = self.now_seen
-        self.tracked_objects[obj_id].features = features
+        # self.tracked_objects[obj_id].features = features
+        self.tracked_objects[obj_id].features_buffer.append(features)
+        if len(self.tracked_objects[obj_id].features_buffer) > 5:
+            self.tracked_objects[obj_id].features_buffer.pop(0)
+            self.tracked_objects[obj_id].features = torch.mean(torch.stack(self.tracked_objects[obj_id].features_buffer), dim=0)
     
     def match_list(self, frame_features, boxes):
         """
@@ -150,8 +156,7 @@ class IDTracker:
     
     def match(self, features, bbox):
         # Compare the features with the tracked objects
-        HARD_THRESH = 0.1
-        SOFT_THRESH = 0.3
+        HARD_THRESH = 0.3
 
         past_features = []
         obj_ids = []
@@ -171,12 +176,12 @@ class IDTracker:
         if min_dist.item() < HARD_THRESH:
             return obj_ids[min_idx.item()]
         
-        if min_dist.item() < SOFT_THRESH:
-            return obj_ids[min_idx.item()]
-            # check if the bbox is similar
-            matched_box = self.tracked_objects[obj_ids[min_idx.item()]].bbox
-            if self.box_iou(bbox, matched_box) > 0.5:
-                return obj_ids[min_idx.item()]
+        # if min_dist.item() < SOFT_THRESH:
+        #     return obj_ids[min_idx.item()]
+        #     # check if the bbox is similar
+        #     matched_box = self.tracked_objects[obj_ids[min_idx.item()]].bbox
+        #     if self.box_iou(bbox, matched_box) > 0.5:
+        #         return obj_ids[min_idx.item()]
         return None
 
     def compare_features(self, features1, features2):
@@ -215,8 +220,22 @@ def tet_video(video_path):
     cap.release()
     cv2.destroyAllWindows()
 
-def test_images():
-    pass
+def test_images(img_dir:str, range:tuple[int, int]):
+    import os
+    img_paths = [os.path.join(img_dir, f) for f in os.listdir(img_dir) if f.endswith('.jpg')]
+    # sort the image paths
+    img_paths.sort(key=lambda x: int(x.split("/")[-1].split(".")[0].split("_")[-1]))
+    img_paths = img_paths[range[0]:range[1]]
+    id_tracker = IDTracker()
+    for img_path in img_paths:
+        img = cv2.imread(img_path)
+        id_tracker.track(img)
+        img_with_boxes = id_tracker.draw_boxes(img)
+        cv2.imshow("ID Tracker", img_with_boxes)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    tet_video("dataset/start.mp4")
+    # tet_video("dataset/start.mp4")
+    test_images("dataset/images/start", (0, 300))
--- people_extractor.py
+++ people_extractor.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class PeopleExtractor(object):
     def __init__(self):
-        self.model = YOLO("models/yolo11n.pt")  # Load a pretrained YOLOv8 model
+        self.model = YOLO("models/yolo11m-seg.pt")  # Load a pretrained YOLOv8 model
         self.model.fuse()  # Fuse model layers for faster inference
         self.model.to("cuda")  # Move the model to GPU for faster inference
 
@@ -12,31 +12,25 @@ class PeopleExtractor(object):
     def extract(self, original_image):
         results = self.model(original_image, conf=0.5, verbose=False)  # Perform inference on the image
         boxes = results[0].boxes.xyxy.cpu().numpy()  # Get bounding boxes
-        # long_boxes = (boxes[:,3] - boxes[:,1])>300  # Calculate the height of the bounding boxes
-        # boxes[long_boxes,3] = boxes[long_boxes,1] + 0.5*(boxes[long_boxes,3] - boxes[long_boxes,1])  # Adjust the height of the bounding boxes
-        # # get classes
         classes = results[0].boxes.cls.cpu().numpy()
         # get confidence
         confs = results[0].boxes.conf.cpu().numpy()
         # Filter boxes for people (class 0)
         boxes = boxes[classes == 0]
         confs = confs[classes == 0]
-        classes = classes[classes == 0]
-        # return np.hstack((boxes, confs[:, np.newaxis], classes[:, np.newaxis]))  # Combine boxes, confidence, and classes
         # compute human images
         imgs = []
         for box in boxes:
             x1, y1, x2, y2 = box
             person_image = original_image[int(y1):int(y2), int(x1):int(x2)]
+            #TODO 将非mask区域设为0
             imgs.append(person_image)
         return boxes, imgs
     def draw_boxes(self, image, boxes):
 
         for box in boxes:
-            x1, y1, x2, y2, conf, cls = box
+            x1, y1, x2, y2 = box
             cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            cv2.putText(image, f'Person: {conf:.2f}', (int(x1), int(y1) - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         return image
     
 if __name__ == "__main__":
@@ -48,7 +42,7 @@ if __name__ == "__main__":
         if not ret:
             break
 
-        boxes = people_extractor.extract(frame)
+        boxes, _ = people_extractor.extract(frame)
         frame_with_boxes = people_extractor.draw_boxes(frame, boxes)
 
         cv2.imshow("People Detection", frame_with_boxes)
