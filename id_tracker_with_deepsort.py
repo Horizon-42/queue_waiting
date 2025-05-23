80f7e103e6c3928fe26e8dc3f6327e972aefba9a
ldx <ldx@xiaofutech.com>
2025-05-23 19:01:55 +0200

    feat(waitting_queue): implement Person class and ThreadSafeOrderedDict for managing waiting queue

--- id_tracker_with_deepsort.py
+++ id_tracker_with_deepsort.py
@@ -2,7 +2,7 @@ import cv2
 from deep_sort_realtime.deepsort_tracker import DeepSort
 from people_extractor import PeopleExtractor
 from feature_extractor import FeatureExtractor
-
+# from the
 
 class IDTracker:
     def __init__(self, camera_id:int):
@@ -10,4 +10,26 @@ class IDTracker:
         self.people_extractor = PeopleExtractor()
         self.feature_extractor = FeatureExtractor()
         # 初始化 Deep SORT
-        self.tracker = DeepSort(max_age=30, n_init=3, nms_max_overlap=1.0)
+        self.tracker = DeepSort(max_age=60, n_init=3, nms_max_overlap=1.0)
+    
+    def process_frame(self, frame):
+        # YOLOv5 目标检测（只检测人类）
+        detections, poses = self.people_extractor.extract_boxes_with_poses(frame)
+        # make images
+        person_images = [frame[int(y1):int(y2), int(x1):int(x2)] for x1,y1,x2,y2, conf, cls in detections]
+        batch_features = [self.feature_extractor.extract(person_image).flatten() for person_image in person_images]
+
+        # 提取所有人的边界框和置信度
+        dets_for_tracker = []
+        for *box, conf, cls in detections:
+            x1, y1, x2, y2 = map(int, box)
+            dets_for_tracker.append(([x1, y1, x2 - x1, y2 - y1], conf.item(), 'person'))
+        
+        tracks = self.tracker.update_tracks(dets_for_tracker, frame=frame, embeds=batch_features)
+
+        for track in tracks:
+            if not track.is_confirmed():
+                continue
+            
+            
+
---
+++ waitting_queue.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from collections import OrderedDict
+from threading import Lock
+import numpy as np
+
+@dataclass
+class Person:
+    ID:int
+    InTime:int
+    OutTime:int
+    IsWaitting:bool
+
+    CrossFeature:np.ndarray
+
+
+class ThreadSafeOrderedDict:
+    def __init__(self):
+        self.od = OrderedDict()
+        self.lock = Lock()
+
+    def set(self, key, value:Person):
+        with self.lock:
+            self.od[key] = value
+
+    def get(self, key):
+        with self.lock:
+            return self.od.get(key)
+
+    def remove(self, key):
+        with self.lock:
+            self.od.pop(key, None)
+
+    def pop_first(self):
+        with self.lock:
+            return self.od.popitem(last=False)
+
+    def __contains__(self, key):
+        with self.lock:
+            return key in self.od
+    
+    def __len__(self):
+        with self.lock:
+            return len(self.od)
+
+    def keys(self):
+        with self.lock:
+            return list(self.od.keys())
+        
+
